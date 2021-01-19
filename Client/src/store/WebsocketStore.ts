76bdd3188be8cf700813640927f9587fab6bc2a0
Mike <depiesms22@gmail.com>
2021-01-18 22:54:46 -0600

    fix auth issues

--- Client/src/store/WebsocketStore.ts
+++ Client/src/store/WebsocketStore.ts
@@ -3,18 +3,13 @@ import { WEBSOCKET } from "@app/api/Config"
 import { lift } from "@app/api/Store/nestedReadable"
 import type { ManagedWebSocket } from "@app/api/types/ManagedWebSocket"
 import type { WebSocketFeature } from "@app/api/WebSocket/WebSocketFeature"
-import { derived } from "svelte/store"
+import { derived, readable } from "svelte/store"
 import { createManagedWebsocket } from "../api/WebSocket/WebSocketCore"
 
-export const auth = createAuth()
-export const managedWebsocket = derived(auth.authData, (authData, set : (v : ManagedWebSocket | undefined) => void) => {
-  if (authData) {
-    const ws = createManagedWebsocket(WEBSOCKET, [createClientIdFeature("dashboard")])
-    set(ws)
-  } else {
-    set(undefined)
-  }
-}, undefined)
+// export const auth = createAuth()
+export const managedWebsocket = readable(createManagedWebsocket(WEBSOCKET, [createClientIdFeature("dashboard")]), start => {
+  
+})
 export const webSocket = lift<ManagedWebSocket, "websocket">(managedWebsocket, "websocket")
 export const message = lift<ManagedWebSocket, "message">(managedWebsocket, "message")
 
