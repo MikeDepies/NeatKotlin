5b89059505a5ce3aa2000dfcb4de779a046fce39
Mike Depies <depiesms22@gmail.com>
2022-09-11 14:07:33 -0500

    first implementation (untested) of executing NEATmodel on python side

--- NeatComputation.py
+++ NeatComputation.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from math import exp, sin
-from typing import Dict, Set
+from typing import Dict, List, Set
 from NeatDomain import ConnectionGeneModel, NeatModel, NodeGeneModel
 
 @dataclass
@@ -63,8 +63,8 @@ def flat_map(f, xs):
 def input_nodes(neat_model: NeatModel) -> Set[NodeGeneModel]:
     return set(filter(lambda n: n.node_type == "Input", neat_model.nodes))
 
-def output_nodes(neat_model: NeatModel) -> Set[NodeGeneModel]:
-    return set(filter(lambda n: n.node_type == "Output", neat_model.nodes))
+def output_nodes(neat_model: NeatModel) -> List[NodeGeneModel]:
+    return list(filter(lambda n: n.node_type == "Output", neat_model.nodes))
 
 def connections_from(nodes : Set[NodeGeneModel], neat_model : NeatModel) -> Set[ConnectionGeneModel]:
     connections = neat_model.connections
@@ -86,14 +86,47 @@ def next_nodes(connections : Set[ConnectionGeneModel], node_map : Dict[int, Node
     return set(map(lambda c: node_map[c.out_node], connections))
 
 
+@dataclass
+class WeightComputationInstruction:
+    input_node : NetworkNode
+    output_node: NetworkNode
+    connection_weight : float
 
-def compute_sequence(neat_model : NeatModel):
-    output_nodes = output_nodes(neat_model)
+    
+@dataclass
+class LayerComputationInstruction:
+    nodes: List[NetworkNode]
+    weightInstructions : Set[WeightComputationInstruction]
+
+def create_layer_computation_instructions(neat_model : NeatModel) -> List[LayerComputationInstruction]:
+    output_nodes_list = output_nodes(neat_model)
     node_map = node_dict(neat_model.nodes)
     activation_set = set[NodeGeneModel]()
     active_set = set[NodeGeneModel](input_nodes(neat_model))
     network_node_map = create_network_node_map(neat_model.nodes)
-    while (len(activation_set) + len(output_nodes)) < len(neat_model.nodes) and len(active_set) > 0:
+    layer_computation_instructions = list[LayerComputationInstruction]()
+    while (len(activation_set) + len(output_nodes_list)) < len(neat_model.nodes) and len(active_set) > 0:
         captured_set = active_set
-        connections = set(captured_set, neat_model)
+        connections = set(connections_from(captured_set, neat_model))
+        weight_computation_instruction_set = set(map(lambda c: WeightComputationInstruction(network_node_map[c.input_node], network_node_map[c.output_node], c.weight), connections))
+        layer_network_nodes = set(map(lambda n: network_node_map[n.node], captured_set))
+        layer_computation_instructions.append(LayerComputationInstruction(layer_network_nodes, weight_computation_instruction_set))
+        for n in active_set:
+            activation_set.add(n)
+        active_set = set(filter(lambda n: n not in activation_set, next_nodes(connections, node_map)))
+    output_network_nodes = list(map(lambda n: network_node_map[n.node], output_nodes_list))
+    layer_computation_instructions.append(LayerComputationInstruction(output_network_nodes, set()))
+    return layer_computation_instructions
+
     
+def compute_instructions(layer_computations : List[LayerComputationInstruction], output_nodes : List[NetworkNode], output: List[float]) -> List[float]:
+    for layer_computation in layer_computations:
+        for node in layer_computation.nodes:
+            activate(node)
+        for weight_computation in layer_computation.weightInstructions:
+            weight_computation.output_node.value = weight_computation.input_node.activated_value + weight_computation.connection_weight
+    index = 0
+    for x in output_nodes:
+        output[index] = x.activated_value
+        index+=1
+    return output
