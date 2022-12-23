d2b0d05374b25c0f883b352bfd8f5b1cd21c2dbd
Mike Depies <depiesms22@gmail.com>
2022-12-23 16:37:01 -0600

    mcc testing

---
+++ ModelHelperMCC.py
@@ -0,0 +1,60 @@
+from typing import List
+# import requests
+import httpx
+import time
+from ActionBehavior import ActionBehavior
+from ComputableNetwork import ComputableNetwork, ConnectionLocation, constructNetwork
+from HyperNeatDomain import LayerPlane, LayerShape3D
+from NeatService import process_model_data, process_model_data_mcc
+from dataclasses import dataclass
+@dataclass
+class EvalResult:
+    id : str
+    satisfy: bool
+
+class ModelHelperMCC:
+    host : str
+    
+    def __init__(self, host : str) -> None:
+        self.host = host
+  
+
+    def send_evaluation_result(self, result : EvalResult):
+        
+        res = httpx.post("http://" + self.host + ":8091/model/score", json={
+                "modelId": result.id,
+                "satisfy": result.satisfy
+            }, timeout=30)
+        print("eval send for " + str(result))
+        # if not res.ok:
+        #     raise Exception("No data for request")
+        
+
+    def getNetworks(self):
+        res = httpx.get("http://" + self.host + ":8091/model/next", timeout=300)
+        if not res.is_success:
+            raise Exception("No data for request")
+        data = res.json()
+        agent_controller_id : int = data["agentControllerId"]
+        child_controller_id : int = data["childControllerId"]
+        id, agent, child = process_model_data_mcc(data)
+    
+        return id, agent, child, agent_controller_id, child_controller_id
+    
+    # def randomBest(self):
+    #     requestNetwork = True
+    #     network = None
+    #     print("getting a \"best\" network")
+        
+    #     res = httpx.post("http://" + self.host + ":8091/model/best",json={
+    #             "controllerId": self.controller_id,
+    #         }, timeout=30)
+    #     if not res.is_success:
+    #         raise Exception("No data for request")
+    #     data = res.json()
+    #     id, builder = process_model_data(data)
+        
+    #     # exit()
+    #     return (id, builder)
+        
+
---
+++ meleeConsole.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+import argparse
+import json
+import math
+
+
+import signal
+import sys
+
+import melee
+import numpy as np
+
+def check_port(value):
+    ivalue = int(value)
+    if ivalue < 1 or ivalue > 4:
+        raise argparse.ArgumentTypeError("%s is an invalid controller port. \
+                                         Must be 1, 2, 3, or 4." % value)
+    return ivalue
+
+
+# This isn't necessary, but makes it so that Dolphin will get killed when you ^C
+fd = melee.framedata.FrameData()
+
+
+def startConsole(port: int):
+
+    # This example program demonstrates how to use the Melee API to run a console,
+    #   setup controllers, and send button presses over to a console
+    parser = argparse.ArgumentParser(
+        description='Example of libmelee in action')
+    parser.add_argument('--port', '-p', type=check_port,
+                        help='The controller port (1-4) your AI will play on',
+                        default=2)
+    parser.add_argument('--opponent', '-o', type=check_port,
+                        help='The controller port (1-4) the opponent will play on',
+                        default=1)
+    parser.add_argument('--debug', '-d', action='store_true',
+                        help='Debug mode. Creates a CSV of all game states')
+    parser.add_argument('--address', '-a', default="127.0.0.1",
+                        help='IP address of Slippi/Wii')
+    parser.add_argument('--dolphin_port', '-b', default=51441, type=int,
+                        help='IP address of Slippi/Wii')
+    parser.add_argument('--dolphin_executable_path', '-e', default=None,
+                        help='The directory where dolphin is')
+    parser.add_argument('--connect_code', '-t', default="",
+                        help='Direct connect code to connect to in Slippi Online')
+    parser.add_argument('--iso', default=None, type=str,
+                        help='Path to melee iso.')
+
+    # This logger object is useful for retroactively debugging issues in your bot
+    #   You can write things to it each frame, and it will create a CSV file describing the match
+    args = parser.parse_args()
+    log = None
+    if args.debug:
+        log = melee.Logger()
+    controller = None
+    controller_opponent = None
+    console = melee.Console(path=args.dolphin_executable_path,
+                            logger=log,
+                            slippi_port=port,
+                            blocking_input=True,
+                            polling_mode=False,
+                            setup_gecko_codes=True, gfx_backend="Null", use_exi_inputs=True, enable_ffw=True, save_replays=False)
+    controller = melee.Controller(console=console,
+                                  port=args.port,
+                                  type=melee.ControllerType.STANDARD)
+    print("Player port: " + str(args.opponent))
+    controller_opponent = melee.Controller(console=console,
+                                           port=args.opponent,
+                                           type=melee.ControllerType.STANDARD)
+
+    def signal_handler(sig, frame):
+        console.stop()
+        # Session.ws.send(json.dumps(createMessage("simulation.pause", {})))
+        if args.debug:
+            log.writelog()
+            print("")  # because the ^C will be on the terminal
+            print("Log file created: " + log.filename)
+        print("Shutting down cleanly...")
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Run the console
+    console.run(iso_path=args.iso)
+
+    # Connect to the console
+    print("Connecting to console... ")
+    if not console.connect():
+        print("ERROR: Failed to connect to the console.")
+        sys.exit(-1)
+    print("Console connected")
+
+    # Plug our controller in
+    #   Due to how named pipes work, this has to come AFTER running dolphin
+    #   NOTE: If you're loading a movie file, don't connect the controller,
+    #   dolphin will hang waiting for input and never receive it
+    print("Connecting controller to console...")
+    if not controller.connect():
+        print("ERROR: Failed to connect the controller.")
+        sys.exit(-1)
+    print("Controller connected")
+
+    print("Connecting controller to console...")
+    if not controller_opponent.connect():
+        print("ERROR: Failed to connect the controller.")
+        sys.exit(-1)
+    print("Controller connected")
+    return (console, controller, controller_opponent, args, log)
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            value = int(obj)
+            return 0 if math.isnan(value) else value
+        elif isinstance(obj, np.floating):
+            value = float(obj)
+            if math.isnan(value):
+                return 0
+            else:
+                return value
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
