92e27e7fa66689ad67be4d8213b281b6202b97dc
Mike Depies <depiesms22@gmail.com>
2022-12-26 11:41:51 -0600

    mcc testing

--- Server/src/main/kotlin/server/message/endpoints/PopulationEvolver.kt
+++ Server/src/main/kotlin/server/message/endpoints/PopulationEvolver.kt
@@ -60,12 +60,12 @@ class PopulationEvolver(
 
 fun mutationDictionary(): List<MutationEntry> {
     return listOf(
-        .9f chanceToMutate getMutateConnections(.1f, .01f, 7f),
-        .0001f chanceToMutate mutateAddNode,
-        .0002f chanceToMutate mutateAddConnection,
-        .9f chanceToMutate getMutateBiasConnections(.1f, .01f, 7f),
-        .0002f chanceToMutate mutateToggleConnection,
-        .0002f chanceToMutate mutateNodeActivationFunction(),
+        .9f chanceToMutate getMutateConnections(.1f, .1f, 7f),
+        .01f chanceToMutate mutateAddNode,
+        .02f chanceToMutate mutateAddConnection,
+        .9f chanceToMutate getMutateBiasConnections(.1f, .1f, 7f),
+        .02f chanceToMutate mutateToggleConnection,
+        .02f chanceToMutate mutateNodeActivationFunction(),
     )
 }
 
