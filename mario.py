c16e9b1ffb4c8f235c978d8eed12f49b240d89bf
Mike Depies <depiesms22@gmail.com>
2023-01-18 09:09:22 -0600

    mcc cpu smash

--- mario.py
+++ mario.py
@@ -609,7 +609,7 @@ if __name__ == '__main__':
     # ns = mgr.Namespace()
     # host = "localhost"
     # port = 8095
-    process_num = 10
+    process_num = 20
     queue = mgr.Queue(process_num * 1)
     processes: List[mp.Process] = []
 
