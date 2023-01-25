28035be298e6cd98e432faf64853900bb1cb6497
Mike Depies <depiesms22@gmail.com>
2023-01-25 09:42:09 -0600

    mcc cpu smash

--- example-base-mcc-cpu.py
+++ example-base-mcc-cpu.py
@@ -279,14 +279,14 @@ if __name__ == '__main__':
     # ns = mgr.Namespace()
     # host = "localhost"
     # port = 8095
-    process_num = 20
+    process_num = 15
     r = get("http://192.168.0.100:8091/configuration")
     data = r.json()
     configuration = processConfiguration(data)
 
     processes: List[mp.Process] = []
     queue_1 = mgr.Queue(process_num)
-    queue_result = mgr.Queue(process_num * 5)
+    queue_result = mgr.Queue(process_num)
 
     p = mp.Process(target=httpRequestProcess, daemon=True,
                    args=(queue_result, ))
