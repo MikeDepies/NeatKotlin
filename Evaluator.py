a1871de1e9f843f2b42566ca7dba8a0bfcc14f1b
Mike Depies <depiesms22@gmail.com>
2023-01-30 13:20:16 -0600

    time penalty for action

--- Evaluator.py
+++ Evaluator.py
@@ -296,13 +296,13 @@ class Evaluator:
                     self.damage_action_available = False
                 self.last_damage_action = player.action
             
-            # if self.frame_data.is_bmove(game_state.players[self.player_index].character, game_state.players[self.player_index].action) or self.frame_data.is_attack(game_state.players[self.player_index].character, game_state.players[self.player_index].action) or self.frame_data.is_grab(game_state.players[self.player_index].character, game_state.players[self.player_index].action):
-            #     self.frames_without_damage += .5
+            if self.frame_data.is_bmove(game_state.players[self.player_index].character, game_state.players[self.player_index].action) or self.frame_data.is_attack(game_state.players[self.player_index].character, game_state.players[self.player_index].action) or self.frame_data.is_grab(game_state.players[self.player_index].character, game_state.players[self.player_index].action):
+                self.frames_without_damage += 2
             
             # if self.frame_data.is_roll(player.character, player.action) or self.frame_data.is_shield(player.action):
             #     self.frames_without_damage += 6
             if self.previous_frame and self.previous_frame.players[self.player_index].action != player.action:
-                self.frames_without_damage += 5
+                # self.frames_without_damage += 5
                 self.damage_action_available = True
                 if self.capture_action(player) and on_stage:
                     # print("prev actions:")
