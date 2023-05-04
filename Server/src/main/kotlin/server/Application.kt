08a6fb4b00ba37814448c2fc145460f8a46e05c6
Mike Depies <depiesms22@gmail.com>
2023-05-04 11:37:01 -0500

    increased process number

--- Server/src/main/kotlin/server/Application.kt
+++ Server/src/main/kotlin/server/Application.kt
@@ -608,7 +608,7 @@ fun simulationFor(controllerId: Int, populationSize: Int, loadModels: Boolean): 
     val randomSeed: Int = 12 + controllerId
     val random = Random(randomSeed)
     val addConnectionAttempts = 5
-    val shFunction = shFunction(.3f)
+    val shFunction = shFunction(.5f)
 
 
     val activationFunctions = Activation.CPPN.functions + ActivationGene("abs") {it.absoluteValue}
--- example-base-selfplay-local-real.py
+++ example-base-selfplay-local-real.py
@@ -413,7 +413,7 @@ if __name__ == '__main__':
     # ns = mgr.Namespace()
     # host = "localhost"
     # port = 8095
-    process_num = 25
+    process_num = 30
     r = get("http://192.168.0.100:8091/configuration")
     data = r.json()
     configuration = processConfiguration(data)
