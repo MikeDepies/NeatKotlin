543bbb5e6b3ca73c5ab58fed22061a2bff79f402
Mike Depies <depiesms22@gmail.com>
2023-06-20 16:07:47 -0500

    more changes

--- Server/src/main/kotlin/server/Simulation2.kt
+++ Server/src/main/kotlin/server/Simulation2.kt
@@ -115,9 +115,9 @@ fun createNetwork(): TaskNetworkBuilder {
 //    val plane4 = layerPlane(15, 15)
 //    val plane5 = layerPlane(15, 15)
     val inputPlanes = listOf(inputPlane/*, inputPlane2, inputPlaneProjectile, inputPlaneController, inputStage*/)
-    val hiddenPlanes = (0..4).map {
+    val hiddenPlanes = (0..2).map {
 //        if (it < 2) layerPlane(12, 12) else
-        layerPlane(9,9)
+        layerPlane(5,5)
     }
     val analogPlane = layerPlane(9, 9)
     val analogCPlane = layerPlane(9, 9)
